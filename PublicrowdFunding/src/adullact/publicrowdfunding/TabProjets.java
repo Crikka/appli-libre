e9f8cb4f14ea38f101dd3fbf00afb2cbc659abd9
lucas34 <lucas34990@gmail.com>
2014-05-07 21:49:21 +0200

    affiche des projets

--- PublicrowdFunding/src/adullact/publicrowdfunding/TabProjets.java
+++ PublicrowdFunding/src/adullact/publicrowdfunding/TabProjets.java
@@ -1,12 +1,18 @@
 package adullact.publicrowdfunding;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import adullact.publicrowdfunding.requester.ServerEmulator;
 import adullact.publicrowdfunding.shared.Project;
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 public class TabProjets extends Activity {
@@ -28,5 +34,39 @@ public class TabProjets extends Activity {
 		HashMap<String, Project> projets = serveur.getAllProjets();
 
 		
+		
+		
+		 //Création de la ArrayList qui nous permettra de remplire la listView
+        ArrayList<HashMap<String, String>> listItem = new ArrayList<HashMap<String, String>>();
+ 
+
+		
+		for (Entry<String, Project> entry : projets.entrySet()) {
+			String titre = entry.getKey();
+			Project projet = entry.getValue();
+        
+	        //On déclare la HashMap qui contiendra les informations pour un item
+	        HashMap<String, String> map;
+	        map = new HashMap<String, String>();
+	        map.put("titre", titre);
+	        map.put("description", projet.name());
+	        listItem.add(map);
+		}
+        
+        
+ 
+       
+        SimpleAdapter mSchedule = new SimpleAdapter (
+        		this.getBaseContext(), 
+        		listItem, 
+        		R.layout.projet_list,
+        		new String[] {"titre", "description"}, 
+        		new int[] {R.id.titre_projet_liste, R.id.description_projet_liste});
+ 
+        //On attribut à notre listView l'adapter que l'on vient de créer
+        listeProjets.setAdapter(mSchedule);
+		
+		
 	}
+
 }
