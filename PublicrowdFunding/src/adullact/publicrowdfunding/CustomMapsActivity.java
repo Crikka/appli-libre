17b8f07ee447b1a300418ca624be417444a62c37
lucas34 <lucas34990@gmail.com>
2014-05-09 01:42:17 +0200

    A toi de voir pour Gmaps car de mon coté ça ne fonctionne pas du tout

--- PublicrowdFunding/src/adullact/publicrowdfunding/CustomMapsActivity.java
+++ PublicrowdFunding/src/adullact/publicrowdfunding/CustomMapsActivity.java
@@ -1,18 +1,24 @@
 package adullact.publicrowdfunding;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 
-public class CustomMapsActivity extends Activity {
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.maps.MapActivity;
+
+public class CustomMapsActivity extends FragmentActivity {
+	GoogleMap map;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.maps);
+
+
 	}
-
-
 
 }
